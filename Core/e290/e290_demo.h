f5a9e4c48983de31df854e43adf45dcc89f0339e
syed93uthman <syed_uthman@yahoo.com>
2025-12-12 16:05:14 +0800

    add e290,pan_driver and cmake setup

---
+++ Core/e290/e290_demo.h
@@ -0,0 +1,35 @@
+#ifndef E290_DEMO_H
+#define E290_DEMO_H
+
+#include "main.h"
+#include "stm32f1xx_hal.h"
+#include "tim.h"
+#include "string.h"
+#include "stdbool.h"
+
+typedef struct
+{
+	int ChirpIoT_sf;
+	int ChirpIoT_bw;
+	int ChirpIoT_cr;
+	int frequency_mhz;
+	int tx_power;
+	int tx_count;
+}pan_config_t;
+
+extern pan_config_t user_config;
+
+// Hardware abstraction functions
+void TIM_Delay_us(uint32_t us);
+
+// E290 demo functions
+bool e290_demo_check_rx_done( uint8_t *buffer, uint8_t * length  , double *rssi);
+void e290_demo_receive( void );
+void e290_demo_transmit( uint8_t *buffer , uint8_t length );
+
+
+// E290 function
+void e290_demo_config_init(void);
+void e290_demo_task(void);
+
+#endif
