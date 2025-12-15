cdedac92fd689b866a98084308c5fafd70fb3886
syed93uthman <syed_uthman@yahoo.com>
2025-12-15 15:23:04 +0800

    Add gp02 driver folder and file.

---
+++ Drivers/GP02/gp02.c
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * @note Copyright (C) 2025 Kemalak Group Sdn Bhd. All rights reserved.
+ *
+ * @file pan_rf.h
+ * @brief
+ * @author Syed Uthman
+ * @author Akmal
+ *
+ * @history - V0.8, 2024-4
+*******************************************************************************/
+
+#include "gp02.h"
